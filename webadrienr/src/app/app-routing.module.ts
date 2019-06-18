5c70e1627b552596ce63130ec10eb9c579c9264d
Adrien RICHARD <richardadrien0@gmail.com>
2019-06-18 23:34:54 +0200

    started angular website

--- webadrienr/src/app/app-routing.module.ts
+++ webadrienr/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  { path: 'map',
+    component: MapComponent,
+  }/*,
+  { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
