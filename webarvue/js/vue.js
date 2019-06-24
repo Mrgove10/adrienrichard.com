99179f96116b95ff3e3b2e20b7c82a58f21e8fbe
Adrien RICHARD <richardadrien0@gmail.com>
2019-06-24 21:38:50 +0200

    added languages

--- webarvue/js/vue.js
+++ webarvue/js/vue.js
@@ -1,5 +1,5 @@
 new Vue({
-    el: '#skill',
+    el: '#progress-content',
     data: {
         skills: [
             {
@@ -8,45 +8,70 @@ new Vue({
                 color: "bar1"
             },
             {
+                title: "JS",
+                percent: "85",
+                color: "bar2"
+            },
+            {
                 title: "C#",
                 percent: "75",
                 color: "bar3"
             },
-
             {
                 title: "PHP & MySQL",
                 percent: "65",
-                color: "bar3"
+                color: "bar4"
             },
             {
-                title: "JS",
-                percent: "85",
-                color: "bar3"
-            },
-            {
-                title: "symfony",
-                percent: "40",
-                color: "bar3"
+                title: "Angular",
+                percent: "65",
+                color: "bar5"
             },
             {
                 title: "Xamarin.Forms",
                 percent: "65",
-                color: "bar3"
+                color: "bar6"
+            },
+            {
+                title: "Symfony",
+                percent: "40",
+                color: "bar7"
             },
             {
                 title: "Arduino",
                 percent: "60",
-                color: "bar3"
+                color: "bar8"
             },
             {
                 title: "Asp.net",
                 percent: "30",
-                color: "bar3"
+                color: "bar9"
+            }
+        ]
+    }
+})
+
+new Vue({
+    el: '#language-skill',
+    data: {
+        langs: [
+            {
+                titlefr: "Anglais",
+                titleen: "English",
+                percent: "100",
+                color: ""
             },
             {
-                title: "Angular",
-                percent: "90",
-                color: "bar3"
+                titlefr: "Francais",
+                titleen: "French",
+                percent: "100",
+                color:"percent1"
+            },
+            {
+                titlefr: "Espagnol",
+                titleen: "Spanish",
+                percent: "40",
+                color:"percent2"
             }
         ]
     }
