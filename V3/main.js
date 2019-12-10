d108b87051338d6756f933bb7cba1d4993f3ef1a
Adrien RICHARD <richardadrien0@gmail.com>
2019-12-10 21:05:30 +0100

    more functions

--- V3/main.js
+++ V3/main.js
@@ -23,6 +23,12 @@ input.keypress(function (e) {
 		} else if (inputVal == "clear") {
 			clear();
 			input.val('');
+		} else if (inputVal == "projects") {
+			projects();
+			input.val('');
+		} else if (inputVal == "experiences") {
+			projects();
+			input.val('');
 		} else if (inputVal == "time") {
 			getTime();
 			input.val('');
@@ -85,18 +91,97 @@ function exit() {
  */
 function help() {
 	var commandsArray = ['Help: List of available commands',
-		'>help',
-		'>about',
-		'>contact',
+		'<span class="green">>about</span>',
+		'<span class="yellow">>contact</span>',
+		'<span class="purple">>projects</span>',
+		'<span class="blue">>experiences</span>',
+		'<span class="orange">>help</span>',
 		'>ping',
 		'>time',
 		'>clear',
 		'>say',
-		'>info'];
+		'>info',
+		'>exit'];
 	for (var i = 0; i < commandsArray.length; i++) {
 		var out = '<span>' + commandsArray[i] + '</span><br/>'
 		Output(out);
 	}
+}
+
+/**
+ * lists the projects i have donne
+ */
+function projects() {
+	var projectArray = ['>Projects:',
+		'<span class="blue"><h3>Furmap.net</h3></span>',
+		'<a href="https://furmap.net">furmap.net</a>',
+		'A world map with more then 500 users !',
+		'<span class="blue"><h3>Patrimoine Grenoblois</h3></span>',
+		'<a href="https://play.google.com/store/apps/details?id=com.AR.grenoble.patrimoine&hl=fr">Play store</a>',
+		'https://github.com/Mrgove10/Hololens-Demo-Salon',
+		'https://github.com/Mrgove10/iowebinterface',
+		'https://github.com/Mrgove10/Mine.Js',
+		'https://github.com/Mrgove10/SGJ2019',
+	]
+	seperator();
+	for (var i = 0; i < projectArray.length; i++) {
+		var out = '<span>' + projectArray[i] + '</span><br/>'
+		Output(out);
+	}
+	seperator();
+}
+
+/**
+ * lists my pro experineces
+ */
+function experiences() {
+	var experiencesArray = ['>Experiences:',
+		'graaly'
+	]
+	seperator();
+	for (var i = 0; i < experiencesArray.length; i++) {
+		var out = '<span>' + experiencesArray[i] + '</span><br/>'
+		Output(out);
+	}
+	seperator();
+}
+
+function aboutMe() {
+	var aboutMeArray = ['>About:',
+		'Hello there!',
+		'I\'m Adrien, Developper (web, apps and video games), tinkerer and diyer.',
+		'Currently studying at EPSI Grenoble (year 3/5)',
+		'<embed src="https://wakatime.com/share/@Adrien/069ebf92-4e0a-4ac3-93bd-516ff27b3bcb.svg" style="height :250px">'];
+	seperator();
+	for (var i = 0; i < aboutMeArray.length; i++) {
+		var out = '<span>' + aboutMeArray[i] + '</span><br/>'
+		Output(out);
+	}
+	seperator();
+}
+
+function contactMe() {
+	var contactArray = [
+		'>Contact:',
+		'[<a href="https://github.com/Mrgove10">GitHub</a>](https://github.com/Mrgove10)',
+		'[<a href="https://www.linkedin.com/in/adrien-rich">Linkedin</a>](https://www.linkedin.com/in/adrien-rich)',
+		'[<a href="mailto:"richardadrien0@gmail.com">Mail</a>](richardadrien0@gmail.com)',
+		'[<a href="https://wakatime.com/@Adrien">Wakatime</a>](https://wakatime.com/@Adrien)'
+	];
+	seperator();
+	for (var i = 0; i < contactArray.length; i++) {
+		var out = '<span>' + contactArray[i] + '</span><br/>'
+		Output(out);
+	}
+	seperator();
+}
+
+/**
+ * Gets the curent time
+ */
+function getTime() {
+	var today = new Date();
+	Output('<span>' + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + '</span></br>');
 }
 
 /**
@@ -137,41 +222,9 @@ function sudo(data) {
 }
 
 /**
- * Gets the curent time
+ * Prints out the result of the command into the output div
+ * @param {*} data 
  */
-function getTime() {
-	//Output('<span>It\'s the 21st century man! Get a SmartWatch</span></br>');
-	var today = new Date();
-	Output(today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
-}
-
-function aboutMe() {
-	var aboutMeArray = ['>About:', 'Hi There!', 'I\'m Mario, a Digital Developer working [@wearecollider](http://www.wearecollider.com) during the day and a designer, freerider, pcbuilder, droneracer and science lover on my free time.', 'Fell free to follow me on twitter @MDesignsuk - see contact page.'];
-	seperator();
-	for (var i = 0; i < aboutMeArray.length; i++) {
-		var out = '<span>' + aboutMeArray[i] + '</span><br/>'
-		Output(out);
-	}
-	seperator();
-}
-
-function contactMe() {
-	var contactArray = [
-		'>Contact:',
-		'[GitHub](https://github.com/Mrgove10)',
-		'[Linkedin](https://bitbucket.org/Mario_Duarte/)',
-		'[Mail](co)',
-		'[Twitter](https://twitter.com/MDesignsuk)'
-	];
-	seperator();
-	for (var i = 0; i < contactArray.length; i++) {
-		var out = '<span>' + contactArray[i] + '</span><br/>'
-		Output(out);
-	}
-	seperator();
-}
-
-// Prints out the result of the command into the output div
 function Output(data) {
 	$(data).appendTo(output);
 }
