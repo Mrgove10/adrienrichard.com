fe176464e0d794ce9338a1cf17387c8e187022ed
Adrien RICHARD <richardadrien0@gmail.com>
2021-06-08 16:59:01 +0200

    better skills

--- CURRENT/site/src/app/home/home.component.ts
+++ CURRENT/site/src/app/home/home.component.ts
@@ -21,6 +21,24 @@ export class HomeComponent implements OnInit {
   public socials = socialsJson;
 
   ngOnInit(): void {
+    // progress bar
+    this.skills.forEach(sk => {
+      let tmpString = '║ ';
+      for (let i = 0; i < 10; i++) {
+        if (i < sk.level) {
+          // create the string
+          tmpString += '██';
+        }
+        else {
+          tmpString += '──';
+        }
+      }
+      tmpString += ' ║';
+      (sk as any).levelstr = tmpString;
+    });
+
+    // skill shuffle
+    this.interests = this.interests.sort(() => .5 - Math.random());
   }
 }
 
